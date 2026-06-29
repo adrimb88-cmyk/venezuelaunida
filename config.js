b0b6b1fdcc862ed726d83855fdf5f5bbd86db7b6
adrimb88-cmyk <adrimb88@gmail.com>
2026-06-29 14:34:35 +0200

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  Rellena tus credenciales de Supabase aquí
 // ============================================================
 
-const SUPABASE_URL = 'https://gsfqjbryitlwmtulcshg.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://gsfqjbryitlwmtulcshg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzZnFqYnJ5aXRsd210dWxjc2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MDM4ODMsImV4cCI6MjA5ODI3OTg4M30.R1hpRrURXeEv-Y6vInB1xgYbX2Hmc_0PpU6VZIG9coc';
 
 // Inicializa el cliente de Supabase
